7403980ba217138ac23c235cb4fdad247f6088f5
SANKABSARMA <sankabsharma12@gmail.com>
2016-12-30 16:14:00 +0530

    motion detection

---
+++ Motion_Detection.py
@@ -0,0 +1,40 @@
+import cv2
+
+def diffimg(a,b,c):
+    t0 = cv2.absdiff(a,b)
+    t1 = cv2.absdiff(b,c)
+
+    t3 = cv2.bitwise_and(t0,t1)
+    return t3
+    
+
+cap = cv2.VideoCapture(0)
+t=cap.read()[1]
+tp=cap.read()[1]
+tpp=cap.read()[1]
+
+t = cv2.cvtColor(t,cv2.COLOR_BGR2GRAY)
+tp = cv2.cvtColor(tp,cv2.COLOR_BGR2GRAY)
+tpp = cv2.cvtColor(tpp,cv2.COLOR_BGR2GRAY)
+
+while True:
+  img = diffimg(t,tp,tpp)
+  cv2.imshow("Motion detection",img) 
+  res,img = cap.read()
+
+  t = tp;
+  tp = tpp;
+  tpp =cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+
+
+
+    
+        
+  key = cv2.waitKey(10)
+
+  if key==27:
+        cv2.destroyAllWindow()
+        break
+
+    
